83c5eb6af1ea5b2fc0cdd0a0afead2652cd60c09
Danilo <danilocalessa@gmail.com>
2017-05-13 16:40:15 -0300

    Criação dos métodos sets na classe Planet

--- Teste/src/enumeravel4/Main.java
+++ Teste/src/enumeravel4/Main.java
@@ -4,10 +4,13 @@ public class Main {
 
 	public static void main(String[] args) {
 
-		Country c1 = new Country(Planet.TERRA, "Terra");
-		Country c2 = new Country(Planet.MARTE, "Marte");
-		Country c3 = new Country(Planet.VENUS, "Vênus");
+		Country c1 = new Country(Planet.TERRA, "Brasil");
+		Country c2 = new Country(Planet.MARTE, "Eua");
+		Country c3 = new Country(Planet.VENUS, "Japão");
 
+		c1.getPlanet().setGravity(555.55f);
+		c1.setName("Islândia");
+		
 		System.out.println(c1.ToString());
 		System.out.println(c2.ToString());
 		System.out.println(c3.ToString());
