e8df4b9cd4ae20d0d5786f4fb4db367b780fa792
Danilo <danilocalessa@gmail.com>
2017-05-13 16:37:55 -0300

    Adição de comentários

--- recursividade/src/recursividade/Fatorial.java
+++ recursividade/src/recursividade/Fatorial.java
@@ -1,16 +1,24 @@
 package recursividade;
 
 public class Fatorial {
-
+	/**
+	 * Calcular o valor fatorial de num
+	 * 
+	 * @param num
+	 * @return
+	 */
 	public static int fat(int num) {
-		if(num==0) {
+		if (num == 0) {
 			return 1;
 		}
-		return num * fat(num-1);
+		return num * fat(num - 1);
 	}
-	
+
+	/**
+	 * Chama o método fat e passa o valor 6 à ser fatorado
+	 */
 	public static void main(String[] args) {
 		Fatorial f = new Fatorial();
-		System.out.println(f.fat(5));
+		System.out.println(f.fat(6));
 	}
 }
