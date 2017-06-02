79cd2e439dfa1b8d0e5827bf44d1084ca8c633fd
ads.2n <ads.2n@L606-18.senacrjedu.local>
2017-06-01 22:07:49 -0300

    comit

---
+++ LinkedStack/src/stack/LinkedStack.java
@@ -0,0 +1,82 @@
+package stack;
+
+import java.util.NoSuchElementException;
+
+public class LinkedStack {
+
+	private Node top;
+	private int size;
+
+	/*
+	 * Contrutor da Clsse
+	 */
+	public LinkedStack() {
+		this.top = null;
+		this.size = 0;
+	}
+
+	/*
+	 * Retorna o tamanho da lista
+	 */
+	public int size() {
+		return this.size;
+	}
+
+	/*
+	 * Retorna true se a lista estiver vazia e false se estiver cheia
+	 */
+	public boolean isEmpty() {
+		return (this.top == null);
+	}
+
+	public void push(int content) {
+		Node oldTop = top;
+
+		top = new Node(content);
+		top.setNext(oldTop);
+		size++;
+
+		// É a mesma coisa
+		// Node newnode = new Node(content);
+		// top = newNode;
+
+	}
+
+	/*
+	 * Implementa o desempilhamento
+	 */
+	public int pop() {
+		// lança uma execessão caso a pilha esteja vazia
+		if (isEmpty()) {
+			throw new NoSuchElementException("A pilha está vazia");
+		}
+
+		Node oldTop = top;
+		int content = oldTop.getContent();
+		top = oldTop.getNext();
+		oldTop.setNext(null);
+		this.size--;
+		return content;
+
+	}
+
+	// verifica a pilha e joga uma excessão caso necessário
+	public int peek() {
+
+		if (isEmpty()) {
+			throw new NoSuchElementException("A pilha está vazia");
+		}
+		return top.getContent();
+	}
+
+	public String toString() {
+		String str = "";
+		Node tmp = top;
+
+		for (int i = 0; i < this.size; i++) {
+			str += tmp.getContent() + "\n";
+			tmp = tmp.getNext();
+		}
+		return str;
+	}
+}
---
+++ LinkedStack/src/stack/Teste.java
@@ -0,0 +1,48 @@
+package stack;
+
+import java.util.NoSuchElementException;
+import java.util.Random;
+
+public class Teste {
+
+	public static void main(String[] args) {
+		/*
+		 * Trata a excessão do método peek da classe LinkedStack
+		 */
+		LinkedStack stack = new LinkedStack();
+		try {
+			stack.peek();
+		} catch (NoSuchElementException e) {
+			System.out.println("A pilha está vazia!!" + "\n" + e.toString());
+		}
+
+		System.out.println("\n");
+
+		Random rand = new Random();
+		/*
+		 * Empilha randomicamente 5 veses
+		 */
+		for (int i = 0; i < 5; i++) {
+			stack.push(rand.nextInt(10));
+		}
+		/*
+		 * Imprimi os métodos
+		 */
+		System.out.println("Peek: " + stack.peek());
+		System.out.println("\n");
+
+		System.out.println(stack.toString());
+
+		System.out.println("\n");
+
+		System.out.println("Pop: " + stack.pop());
+		System.out.println("Pop: " + stack.pop());
+		System.out.println("Pop: " + stack.pop());
+		System.out.println("Pop: " + stack.pop());
+
+		System.out.println("\n");
+
+		System.out.println("Peek: " + stack.peek());
+
+	}
+}
