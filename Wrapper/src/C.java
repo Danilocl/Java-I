bc542d62084c89cf6c2e81f6d2415582bd0be5d5
ads.2n <ads.2n@L606-18.senacrjedu.local>
2017-06-14 22:10:01 -0300

    Comit

--- Wrapper/src/C.java
+++ Wrapper/src/C.java
@@ -1,11 +1,14 @@
 
-public class Main {
+public class C {
 
 	public static void main(String[] args) {
-		int a = 1;
+		int a = 1;		
 		System.out.println(a);
 
-		Integer b = new Integer(2);
+//São a mesma coisa		
+//		Integer b = new Integer(2);
+		Integer b = 2;
+		int c = b;
 		System.out.println(b);
 	}
 }
